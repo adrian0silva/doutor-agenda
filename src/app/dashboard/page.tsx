f515d520e150d72779126c22fcb1146fda288aa1
adrian0silva <adrianosilvaparola@gmail.com>
2025-05-31 15:02:47 -0300

    feat: Adicionar funcionalidade de criação de clínicas e integração com o sistema de autenticação
    - Implementado o formulário de criação de clínicas com validação.
    - Adicionada lógica para redirecionamento ao dashboard após a criação da clínica.
    - Integrado o novo componente de diálogo para a interface de usuário.
    - Atualizado o esquema do banco de dados para suportar a relação entre usuários e clínicas.
    - Adicionadas dependências necessárias no package.json e package-lock.json.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,9 @@
+import { eq } from "drizzle-orm";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import SignOutButton from "./components/sign-out-button";
 const DashboardPage = async () => {
   const session = await auth.api.getSession({
@@ -8,6 +11,12 @@ const DashboardPage = async () => {
   });
   if (!session?.user) {
     redirect("/authentication");
+  }
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
   }
   return (
     <div>
